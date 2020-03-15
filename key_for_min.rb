5bc56dea07737a8e1de541df8d4d7aa496a1c891
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-15 00:48:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  if name_hash. length == 0 
-    return nil 
-  
-    name_hash.each do |key, value| 
-      if min_numb == value 
-        return key 
-      end 
-    end 
-    lowest 
-  end 
-end 
 
-ikea = {:chair =>25, :table => 85, 
-  :mattress => 450} 
-key_for_min_value (ikea) 
 
 
